1ae6cb43f24a6375140873fc461100f788d9d53c
lazarcon <constantin.lazari@gmail.com>
2013-10-17 17:05:49 +0200

    Created InstructionGroups for less complex handling

---
+++ CPU-Simulation/src/main/java/ch/zhaw/lazari/cpu/impl/InstructionGroup.java
@@ -0,0 +1,25 @@
+/*
+ * File: 		InstructionGroup.java
+ * Date: 		Oct 17, 2013
+ *
+ * Copyright 2013 Constantin Lazari. All rights reserved.
+ *
+ * Unless required by applicable law or agreed to in writing, this software
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.
+ */
+package ch.zhaw.lazari.cpu.impl;
+
+/**
+ * Defines available instruction groups
+ */
+public enum InstructionGroup {
+
+	REGISTER,
+	ACCU,
+	MEMORY,
+	ARITHMETIC,
+	LOGICAL,
+	PROGRAM_COUNTER,
+	CPU
+}
