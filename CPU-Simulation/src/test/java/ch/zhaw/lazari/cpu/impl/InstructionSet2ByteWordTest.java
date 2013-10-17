c2e30ea84e3ab948a6f059eb6ddedd0482a80428
lazarcon <constantin.lazari@gmail.com>
2013-10-17 13:07:12 +0200

    Created testable instruction set

---
+++ CPU-Simulation/src/test/java/ch/zhaw/lazari/cpu/impl/InstructionSet2ByteWordTest.java
@@ -0,0 +1,34 @@
+/*
+ * File: 		RegexInterpreterImplTest.java
+ * Date: 		Oct 17, 2013
+ *
+ * Copyright 2013 Constantin Lazari. All rights reserved.
+ *
+ * Unless required by applicable law or agreed to in writing, this software
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.
+ */
+package ch.zhaw.lazari.cpu.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Responsibility:
+ */
+public class InstructionSet2ByteWordTest {
+
+	/**
+	 * Test method for {@link ch.zhaw.lazari.cpu.impl.InstructionSet2ByteWord#contains(java.lang.String)}.
+	 */
+	@Test
+	public void testClear00() {
+		final String word = "0000001010000000";
+		final InstructionSet2ByteWord instruction = InstructionSet2ByteWord.create(word);
+		assertEquals(InstructionSet2ByteWord.CLR, instruction);
+		assertEquals(0, instruction.getRegisterId(word));
+	}
+
+	
+}
